a13f120a6f7efb2f70c27f5be67a9bc19a6124e8
黄艳鹏 <huangyanpeng@sinosoft.com.cn>
2026-01-12 10:26:20 +0800

    1、解决部分低版本客户端无法使用问题

--- password-xl-web/vite.config.ts
+++ password-xl-web/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 const srcPath = path.resolve(__dirname, './src')
 
@@ -21,6 +22,9 @@ export default defineConfig({
     },
     plugins: [
         vue(),
+        legacy({
+            targets: ['Chrome >= 49', 'Android >= 5', 'iOS >= 10'],
+        }),
         AutoImport({
             imports: ['vue'],
             resolvers: [
